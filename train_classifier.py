a0103c4199bffd92ba9291758d004a8b712671a2
A77-2005 <alsaihtiali@gmail.com>
2025-02-11 00:33:02 +0300

    Update train_classifier.py

--- train_classifier.py
+++ train_classifier.py
@@ -1,8 +1,11 @@
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.model_selection import train_test_split
+
 from sklearn.metrics import accuracy_score
+
 import numpy as np
 
 
